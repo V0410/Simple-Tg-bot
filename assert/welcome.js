b90f8142f0baff05c9570b1aba97bbc100d623bf
capybara003 <mantoketo4+1@gmail.com>
2025-03-12 14:22:28 -0700

    add boost

---
+++ assert/welcome.js
@@ -0,0 +1,29 @@
+// welcome message
+const welcomeMessage =
+    `*Welcome to Orbit MM\!* 🚀\n\n` +
+    `Experience the unique power of *Orbit MM's Solana Volume Boosting*\n` +
+    `Service optimized for *Solana* and *Pumpfun*, designed to attract *new organic investors*\n\n` +
+    `*Here's How:*\n` +
+    `⚡ *3 Modes:* Choose between *Fast Mode* \\(up to 6h\\), *Normal Mode* \\(up to 24h\\), or *Steady Mode* \\(up to 7 days\\).\n` +
+    `📦 *Package Selection:* From *9 to 60 SOL*, tailored to your budget.\n` +
+    `🤖 *MicroBots:* Up to *6 MicroBots* per package, each adding *2 tx per minute*.\n` +
+    `😏 *FOMO Creation:* High volume from unique wallets creates *powerful FOMO* for your project.\n` +
+    `✨ *Organic Trending:* High transaction rates drive *natural visibility* on crypto platforms.\n\n` +
+    `*Get Started!*\nBoost your market presence discreetly and effectively with *Orbit MM*.\n`;
+
+// welcome buttons
+const BUTTONS = [
+    [
+        { text: 'Boost Volume 📊', action: 'boost' },
+        { text: 'Referrals 💰', action: 'referrals' }
+    ],
+    [
+        { text: 'Support 🆘', action: 'support' },
+        { text: 'Learn more 🔗', action: 'learn_more' }
+    ]
+];
+
+module.exports = {
+    welcomeMessage,
+    BUTTONS
+}
--- bot.js
+++ bot.js
@@ -1,26 +1,32 @@
+
 const { Telegraf, Markup } = require("telegraf");
+const { welcomeMessage, BUTTONS } = require("./assert/welcome");
+const { boostMessage, POOL_OPTIONS } = require("./assert/boost");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN); // better use .env
 
+
+
 bot.start((ctx) => ctx.reply(
-  `*Welcome to Orbit MM\!* 🚀\n\n` +
-  `Experience the unique power of *Orbit MM's Solana Volume Boosting*\n` +
-  `Service optimized for *Solana* and *Pumpfun*, designed to attract *new organic investors*\n\n` +
-  `*Here's How:*\n` +
-  `⚡ *3 Modes:* Choose between *Fast Mode* \\(up to 6h\\), *Normal Mode* \\(up to 24h\\), or *Steady Mode* \\(up to 7 days\\).\n` +
-  `📦 *Package Selection:* From *9 to 60 SOL*, tailored to your budget.\n` +
-  `🤖 *MicroBots:* Up to *6 MicroBots* per package, each adding *2 tx per minute*.\n` +
-  `😏 *FOMO Creation:* High volume from unique wallets creates *powerful FOMO* for your project.\n` +
-  `✨ *Organic Trending:* High transaction rates drive *natural visibility* on crypto platforms.\n\n` +
-  `*Get Started!*\nBoost your market presence discreetly and effectively with *Orbit MM*.\n` + 
-  ` [Orbit MM Website](https://orbitmm.com) | ` + `[Twitter](https://twitter.com/OrbitMM) | ` + `[Telegram](https://t.me/OrbitMM) | ` + `[Orbit MM Guide](https://orbitmm.com/guide) | ` + `[Whitelabel](https://orbitmm.com/whitelabel)`,
-  Markup.inlineKeyboard([
-    [Markup.button.callback('Boost Volume 📊', 'boost'), Markup.button.callback('Referrals 💰', 'referrals')],
-    [Markup.button.callback('Support 🆘', 'support'), Markup.button.callback('Learn more 🔗', 'learn_more')]
-  ])
+  welcomeMessage,
+  Markup.inlineKeyboard(
+    BUTTONS.map(row =>
+      [row.map(button => `${[Markup.button.callback(button.text, button.action)]}, `)]
+    ),
+    {
+      columns: 2,
+      row_height: 2,
+      resize_keyboard: true,
+      one_time_keyboard: true,
+      selective: true,
+    }
+  )
 )); // will be executed when /start 
+
+bot.action('boost', (ctx) =>
+  ctx.reply(boostMessage, Markup.inlineKeyboard(POOL_OPTIONS.map(option => [Markup.button.callback(option.text, option.action)]))));
 bot.command("help", (ctx) => ctx.reply("Help Works")); // works when you type /help
 bot.on("sticker", (ctx) => // when bot recives a sticker , it reply with a sticker according to the sticker-id mention in replyWithSticker()
   ctx.replyWithSticker(

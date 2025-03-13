097800c0d76cb3d23b5467b2bf78b9a1bdac4242
capybara003 <mantoketo4+1@gmail.com>
2025-03-13 00:53:42 -0700

    update boost
    Co-authored-by: Capybara003 mantoketo4+1@gmail.com
    Co-authored-by: V0410 gru.dev259007@gmail.com

--- assert/boost/index.js
+++ assert/boost/index.js
@@ -4,7 +4,6 @@ const { Markup } = require("telegraf");
 // Define boosting message
 const boost = {
     message: `Select the pool you'd like Orbitt MM to operate in:\n` +
-
         `🟣 Raydium: Orbitt MM will immediately locate your pool and apply optimized volume-boosting settings.\n` +
         `🟢 PumpFun: After your token completes the bonding curve on PumpFun and transitions to Raydium, Orbitt MM will seamlessly continue the volume-boosting process on Raydium.\n` +
         `🟠 Meteora: Orbitt MM will identify your pool on Meteora and apply volume-boosting strategies specifically optimized for the platform.\n` +

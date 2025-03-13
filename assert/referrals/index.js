6db3ffc8c315ee1f692779172519a130b92f1e40
capybara003 <mantoketo4+1@gmail.com>
2025-03-13 00:10:06 -0700

    blah bah
    Co-authored-by: V0410 gru.dev259007@gmail.com
    Co-authored-by: Capybara003 mantoketo4+1@gmail.com

--- assert/referrals/index.js
+++ assert/referrals/index.js
@@ -1,7 +1,6 @@
 
 const { Markup } = require("telegraf");
 
-
 const referrals = {
     message: `➡️ Please provide your Solana wallet address where you'll receive Solana Coins (SOL) straight from purchases made through your link.\n\n` +
         `⚠️ Referral activation takes 24 hours ⚠️`,
